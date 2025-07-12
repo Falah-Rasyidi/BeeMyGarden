20a00a680444771ee30ff0ed6a2e6e3c684ec060
Falah-Rasyidi <s4007133@student.rmit.edu.au>
2025-07-13 00:38:42 +1000

    Initial Commit

---
+++ BeeMyGarden/BeeMyGardenApp.swift
@@ -0,0 +1,33 @@
+//
+//  BeeMyGardenApp.swift
+//  BeeMyGarden
+//
+//  Created by Falah Rasyidi on 13/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeeMyGardenApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}

68df8cb55ffeb4531e2848818ee48249ba506f8f
TIMMMC01 <timcurrie22@gmail.com>
2025-07-14 10:47:21 +1000

    changed the colour to bright pink. Speach.

---
+++ BeeMyGarden/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  BeeMyGarden
+//
+//  Created by Tim Currie on 14/7/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
